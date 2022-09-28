509db60c57fe1547c44343076581368639a3ce05
wgpdb <dariusz.borek@gmail.com>
2022-09-29 01:07:38 +0200

    added checkout component

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup!: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        postCode: [''],
+        city: [''],
+        country: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        postCode: [''],
+        city: [''],
+        country: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cartType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+  }
+
+  copyShippingAddressToBillingAddress(event: { target: { checked: any; }; }) {
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls['billingAddress']
+        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
+    } else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
+  onSubmit() {
+    console.log("Submit handler")
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+}
